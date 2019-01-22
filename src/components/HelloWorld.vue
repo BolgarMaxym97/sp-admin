bf0688b825b12dcc6e9a1f6e185177435281e50a
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-01-22 16:52:09 +0200

    bootstrap vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+      <b-alert show>Test</b-alert>
+      <b-alert variant="success" show>Test2</b-alert>
+      <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
