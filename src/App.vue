01b941867ae67ce6aa29d2985554e47644425d53
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-01-29 14:01:48 +0200

    custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
         <top-bar v-if="isLoggedIn"></top-bar>
         <b-container fluid>
             <b-row>
-                <b-col v-if="isLoggedIn" cols="2" class="left-sidebar-col">
-                    <left-side-bar></left-side-bar>
-                </b-col>
+                <VuePerfectScrollbar class="left-sidebar-col" v-once :settings="scrollBarrSetting">
+                    <b-col v-if="isLoggedIn" cols="2" style="max-width: 100%;">
+                        <left-side-bar></left-side-bar>
+                    </b-col>
+                </VuePerfectScrollbar>
                 <b-col>
                     <router-view/>
                 </b-col>
@@ -17,14 +19,19 @@
 <script>
     import TopBar from "@/components/layout/TopBar";
     import LeftSideBar from "@/components/layout/LeftSideBar";
+    import VuePerfectScrollbar from "vue-perfect-scrollbar";
+    import config from "@/config";
 
     export default {
         data() {
-            return {};
+            return {
+                scrollBarrSetting: config.vueScrollOptions
+            };
         },
         components: {
             TopBar,
             LeftSideBar,
+            VuePerfectScrollbar
         },
         computed: {
             isLoggedIn: function () {
@@ -35,18 +42,19 @@
 </script>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    @import url("https://fonts.googleapis.com/css?family=Roboto");
 
     #app {
         font-family: Roboto, serif;
         height: calc(100% - 56px);
     }
-    
+
     .left-sidebar-col {
         min-height: calc(100vh - 56px);
         max-height: calc(100vh - 56px);
         overflow-y: auto;
         background-color: #272c33;
         padding: 0;
+        width: 300px;
     }
 </style>
--- src/config.js
+++ src/config.js
@@ -3,5 +3,9 @@ export default {
         defaultPosition: "toast-top-right",
         defaultType: "info",
         defaultTimeout: 2000
+    },
+    vueScrollOptions: {
+        wheelSpeed: 0.5,
+        maxScrollbarLength: 60,
     }
 };
