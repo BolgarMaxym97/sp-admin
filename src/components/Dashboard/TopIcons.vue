9b27b1eb33c73d69f5d744b1f1adaf2066446aa5
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-01 12:16:56 +0200

    dashboard icons fetch added

--- src/components/Dashboard/TopIcons.vue
+++ src/components/Dashboard/TopIcons.vue
@@ -1,42 +1,48 @@
 <template>
     <b-row>
-        <top-icon :key="index" v-for="(topCard, index) in topCards" :card="topCard"/>
+        <top-icon :key="index" v-for="(topIcon, index) in topIcons" :icon="topIcon"/>
     </b-row>
 </template>
 
 <script>
     import TopIcon from "@/components/Dashboard/TopIcon";
+    import {ENDPOINTS} from "@/api";
 
     export default {
         data() {
-            return {};
-        },
-        computed:{
-            topCards: function () {
-                return [
-                    {
+            return {
+                topIcons: {
+                    customersCount: {
                         name: "Customers",
                         count: 0,
                         bg_class: "bg-primary-color-6",
                         icon: "users",
                         is_percent: false
                     },
-                    {
+                    nodesCount: {
                         name: "Nodes",
                         count: 0,
                         bg_class: "bg-primary-color-5",
                         icon: "network-wired",
                         is_percent: false
                     },
-                    {
+                    sensorsCount: {
                         name: "Sensors",
                         count: 0,
                         bg_class: "bg-primary-color-3",
                         icon: "thermometer-half",
                         is_percent: false
                     }
-                ];
-            }
+                }
+            };
+        },
+        mounted() {
+            this.$http.get(ENDPOINTS.STATISTIC)
+                .then(statistic => {
+                    this.topIcons.customersCount.count = statistic.customers_count;
+                    this.topIcons.nodesCount.count = statistic.objects_count;
+                    this.topIcons.sensorsCount.count = statistic.sensors_count;
+                });
         },
         components: {
             TopIcon,
