fadd8f6c224e77d40c45bb67c933914c4f5de79c
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-03 18:42:49 +0200

    dashboard chart done

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,8 @@
         <top-icons :customersCount="customersCount" :nodesCount="nodesCount" :sensorsCount="sensorsCount"/>
         <b-row>
             <b-card class="text-center m-auto chart-card">
-                <chart :chartData="chartData"/>
+                <font-awesome-icon v-if="loading" icon="spinner" class="loader" />
+                <chart v-else :chartData="chartData"/>
             </b-card>
         </b-row>
     </div>
@@ -22,6 +23,7 @@
                 sensorsCount: 0,
                 labels: [],
                 data: [],
+                loading: true
             };
         },
 
@@ -33,6 +35,7 @@
                     this.sensorsCount = statistic.sensors_count;
                     this.labels = statistic.chartData.labels;
                     this.data = statistic.chartData.data;
+                    this.loading = false;
                 });
         },
         computed: {
@@ -46,7 +49,15 @@
                             pointBackgroundColor: "#9a5b1c",
                             backgroundColor: "#d4821c",
                             fill: false,
-                            data: this.data.customersDataByDates
+                            data: this.data[0]
+                        },
+                        {
+                            label: "Объекты",
+                            borderColor: "#301846",
+                            pointBackgroundColor: "#221130",
+                            backgroundColor: "#301846",
+                            fill: false,
+                            data: this.data[1]
                         }
                     ],
                 };
@@ -68,6 +79,10 @@
         position: relative;
         width: calc(100% - 30px);
         height: 450px;
+    }
+    .loader {
+        color: #221130;
+        font-size: 48px;
     }
 </style>
 
