41e3ab909469da721543a0f84211c1b80e805c44
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-11 14:27:38 +0200

    loader for confirm modal

--- src/modals/ConfirmModal.vue
+++ src/modals/ConfirmModal.vue
@@ -6,8 +6,11 @@
              ok-title="Да"
              cancel-title="Нет"
              ok-variant="success"
+             :cancel-disabled="loading"
+             :ok-disabled="loading"
              @hidden="onHidden"
              @ok="ok">
+        <font-awesome-icon v-if="loading" icon="spinner" class="loader"/>
         <span v-html="text"></span>
     </b-modal>
 </template>
@@ -24,13 +27,31 @@
                 default: "Вы уверены что хотите сделать это?"
             }
         },
+        data() {
+            return {
+                loading: false
+            };
+        },
         methods: {
             onHidden() {
                 this.$emit("hidden");
             },
             ok(ev) {
+                this.loading = true;
                 this.$emit("onOk", ev);
             }
         }
     };
 </script>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/colors";
+
+    .loader {
+        color: $primary-color-5;
+        position: absolute;
+        top: 0;
+        left: 0;
+        font-size: 64px;
+    }
+</style>
