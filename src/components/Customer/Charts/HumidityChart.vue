210c62512ec50060e65cd28adf1691581bd0b65b
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-12 13:12:56 +0200

    humidity added

---
+++ src/components/Customer/Charts/HumidityChart.vue
@@ -0,0 +1,30 @@
+<script>
+    import {Line, mixins} from "vue-chartjs";
+    import config from "@/config";
+
+    export default {
+        extends: Line,
+        mixins: [mixins.reactiveProp],
+        props: ["chartData"],
+        mounted() {
+            const options = Object.assign(config.defaultOptionsForChartModal, {
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Влажность, %"
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Время"
+                        }
+                    }]
+                }
+            });
+
+            this.renderChart(this.chartData, options);
+        }
+    };
+</script>
