fb79128c59bf9123539c0dac2359921b0d155ead
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-05-10 16:19:56 +0300

    alerts done

---
+++ src/components/Customer/Node/Alerts.vue
@@ -0,0 +1,33 @@
+<template>
+    <span class="node-card__alarms">
+                    <b-badge variant="danger"
+                             class="node-card__alarms-item"
+                             :key="alert"
+                             v-for="alert in alerts">
+                        {{alert}}
+                    </b-badge>
+                </span>
+</template>
+
+<script>
+    export default {
+        props: {
+            alerts: {
+                type: Array,
+                required: true
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .node-card__alarms {
+        display: block;
+        position: absolute;
+        bottom: 135px;
+
+        .node-card__alarms-item {
+            margin: 2px;
+        }
+    }
+</style>
